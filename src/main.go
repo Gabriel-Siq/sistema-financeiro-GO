aa9bb3696d603ef4790115b421e01d6ff9d3284d
Gabriel Siqueira <gabrielsiqueira.gsm@gmail.com>
2024-02-15 23:23:42 -0300

    feat: add method realizaSaque

--- src/main.go
+++ src/main.go
@@ -2,11 +2,37 @@ package main
 
 import "fmt"
 
-func main() {
-	var titular string = "Gabriel"
-	var numAgencia int = 589
-	var numConta int = 123456
-	var saldo float64 = 124.50
+type ContaCorrente struct {
+	titular    string
+	numAgencia int
+	numConta   int
+	saldo      float64
+}
 
-	fmt.Println(titular, numAgencia, numConta, saldo)
+func (conta *ContaCorrente) realizaSaque() {
+	var valorSaque float64
+
+	fmt.Print("Informe o valor que deseja sacar: ")
+	fmt.Scanf("%f", &valorSaque)
+
+	if valorSaque <= conta.saldo {
+		conta.saldo -= valorSaque
+
+		fmt.Print("O valor do saldo atual eh: ", conta.saldo)
+		fmt.Println("")
+	} else {
+		fmt.Println("O valor desejado de saque é maior do que o existente na conta")
+	}
+
+}
+
+func main() {
+
+	contaTheo := ContaCorrente{}
+	contaTheo.titular = "Theo"
+	contaTheo.saldo = 500
+
+	fmt.Println(contaTheo)
+
+	contaTheo.realizaSaque()
 }
